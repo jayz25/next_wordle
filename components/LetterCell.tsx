577ce41a7155eab8fb139e39189433786b0b3082
Jayesh <patiljayesh026@gmail.com>
2024-05-13 00:11:10 +0530

    TS Changes

--- components/LetterCell.tsx
+++ components/LetterCell.tsx
@@ -1,4 +1,6 @@
-export const LetterCell = () => {
+import React from "react"
+
+export const LetterCell: React.FC = () => {
     return (
         <div>
             <input className="p-2 outline h-14 w-14 text-4xl text-center"/>
