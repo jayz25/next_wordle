fbcdd704ebb82f495d1423a1a54b653cf2e7902c
Jayesh <patiljayesh026@gmail.com>
2024-05-13 00:04:21 +0530

    Added basic letter component and some colors we will be using based on values

---
+++ components/LetterCell.tsx
@@ -0,0 +1,7 @@
+export const LetterCell = () => {
+    return (
+        <div>
+            <input className="p-2 outline h-14 w-14 text-4xl text-center"/>
+        </div>
+    )
+}
---
+++ constants/colors.ts
@@ -0,0 +1,5 @@
+const BG_LETTERCELL_GREEN = "#90D26D";
+const BG_LETTERCELL_YELLOW = "#FFF455";
+const WHITE = "#FFFFFF";
+const black ="#000000";
+
